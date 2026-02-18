40946dc55adebf0684fd4b4750fa611db7bdac37
Neuromancer <m.arda.safak06@gmail.com>
2026-02-18 18:24:21 +0300

    Updated README and added 1.c about fork().besides it:I kissed a boy and I liked it The taste of his cherry chapstick I kissed a boy just to try it I hope my girlfriend don't mind it It felt so wrong It felt so right

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 - **This repo is for learning fork() and processes for improve ShittyJukebox project and all the lecture(ish) shit will be in here**
 
 ## Lectures
-- None yet.
+- Added 1.c, about fork()
+- 2.c pending about wait()
+ 
 
 ## Examples 
 - This part is fuck around and find out zone(probably will have a forkbomb for fun too)
---
+++ lectures/1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*                      Introduction for fork() next lecture will be about wait()           */
+
+int main(void)
+{
+
+    int pid = fork();                 //This creates a parallel process line from this point on
+    printf("hello, world!\tProcess ID:%d\n",pid);   //Child process gets 0 as the OG getting the actual ID: IMPORTANT: -1 WHEN AN ERROR OCCURS
+    if (pid == 0)
+    {
+        printf("I've been born!\n");
+    }
+    else
+    {
+        printf("Does that mean I finally got laid somewhere in past?\n");
+    }
+    
+
+    return(0);
+}
